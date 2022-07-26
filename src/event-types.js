dc5ffee3148f779e3089c41a2aa6a09dc1d67ddc
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-07-26 16:25:40 -0400

    Update event types

--- src/event-types.js
+++ src/event-types.js
@@ -2,3 +2,4 @@ export const EVENT_ITEM_NEW = Symbol("EVENT_ITEM_ADD");
 export const EVENT_ITEM_DONE = Symbol("EVENT_ITEM_DONE");
 export const EVENT_LIST_CLEAR = Symbol("EVENT_LIST_CLEAR");
 export const EVENT_DATA_CHANGED = Symbol("EVENT_DATA_CHANGED");
+
