bee65afa9ef4c75ceecf0a24a61a2c212f77cc18
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-07-29 16:28:09 -0400

    Add toggle modal to add todo items

--- src/index.js
+++ src/index.js
@@ -4,7 +4,4 @@ import UI from "./modules/ui";
 // document.addEventListener('DOMContentLoaded', UI.loadSite);
 // window.onload = UI.loadSite();
 
-document.addEventListener("DOMContentLoaded", function(){
-    console.log("Site loaded?");
-    UI.loadSite();
-});
+document.addEventListener("DOMContentLoaded", UI.loadSite);
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,15 +1,72 @@
 const body = document.querySelector("body");
 const todoContainerParent = body;
 
+let isAddTodoVisible = false;
+
 export default class UI {
     static loadSite() {
-        UI.createTodoContainer();
+        UI.createContainerTodo();
+        UI.createContainerAddItem();
     }
 
-    static createTodoContainer() {
+    static createContainerTodo() {
         const todoElement = document.createElement('div');
         todoElement.id = "todo";
         todoContainerParent.append(todoElement);
+    }
+
+    static toggleAddItem() {
+        const addItem = body.querySelector('#add-item-toggle');
+        if(isAddTodoVisible) this.setAddItemDisplayOn(addItem);
+        else this.setAddItemDisplayOff(addItem);
+    }
+
+    static setAddItemDisplayOff(element) {
+        isAddTodoVisible = true;
+        element.style.display = 'block';
+        element.querySelector('#input-item').focus();
+        body.querySelector('#button-toggle-item').textContent = 'highlight_off';
+    }
+
+    static setAddItemDisplayOn(element) {
+        isAddTodoVisible = false;
+        console.dir(element);
+        element.style.display = 'none';
+        body.querySelector('#button-toggle-item').textContent = 'add_circle_outline';
+    }
+
+    static createContainerAddItem() {
+        const addItemMainDiv = document.createElement('div');
+        addItemMainDiv.id = 'add-item';
+        
+        const toggleDiv = document.createElement('div');
+        toggleDiv.id = 'add-item-toggle';
+        toggleDiv.style.display = 'none';
+
+        const inputTextItem = document.createElement('input');
+        inputTextItem.id = 'input-item';
+        inputTextItem.type = 'text';
+
+        const buttonAdd = document.createElement('button');
+        buttonAdd.id = 'button-add';
+        buttonAdd.textContent = "Add";
+
+        // Button to show and hide the add (input / button)
+        const buttonToggle = document.createElement('button');
+        buttonToggle.id = "button-toggle-item";
+        buttonToggle.type = 'image';
+        buttonToggle.classList.add('material-icons');
+        buttonToggle.textContent = 'add_circle_outline';
+        buttonToggle.addEventListener('click', () => {
+            console.log("clicked");
+            UI.toggleAddItem();
+        });
+
+        toggleDiv.append(inputTextItem);
+        toggleDiv.append(buttonAdd);
+        addItemMainDiv.append(toggleDiv);
+        addItemMainDiv.append(buttonToggle);
+        todoContainerParent.append(addItemMainDiv);
     }
 
 }
