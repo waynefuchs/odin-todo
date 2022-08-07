fb8e18ef6c3d3f28a8893d4ff0454aeece9dea4e
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-08-07 00:55:32 -0400

    Testing complete

--- testing/script.js
+++ testing/script.js
@@ -1,74 +1,51 @@
-elements = document.querySelectorAll('.draggable');
-for(const element of elements) {
+// elements = document.querySelectorAll('.draggable');
+// for(const element of elements) {
+//     element.draggable = true;
+//     element.addEventListener('dragstart', dragStart);
+//     element.addEventListener('drop', dragDropped);
+
+//     element.addEventListener('dragenter', dragEnter);
+//     element.addEventListener('dragover', dragOver);
+
+//     element.addEventListener('dragleave', dragLeave);
+// }
+
+const draggables = document.querySelectorAll('.draggable');
+const containers = document.querySelectorAll('.dropzone');
+
+draggables.forEach(element => {
     element.draggable = true;
-    element.addEventListener('dragstart', dragStart);
-    element.addEventListener('drop', dragDropped);
+    element.addEventListener('dragstart', (event) => {
+        element.classList.add('dragging');
+    });
+    element.addEventListener('dragend', (event) => {
+        element.classList.remove('dragging');
+    });
+});
 
-    element.addEventListener('dragenter', dragEnter);
-    element.addEventListener('dragover', dragOver);
 
-    element.addEventListener('dragleave', dragLeave);
-}
+containers.forEach(container => {
+    container.addEventListener('dragover', (event) => {
+        event.preventDefault();
+        const afterElement = getDragAfterElement(container, event.clientY);
+        const element = document.querySelector('.dragging');
+        if(afterElement == null) {
+            container.appendChild(element);
+        } else {
+            container.insertBefore(element, afterElement);
+        }
+    });
+});
 
-let blah;
-
-function dragStart(e) {
-    e.dataTransfer.effectAllowed = "move";
-    const items = Array.from(document.querySelectorAll('div.drag'));
-    e.target.style.display = "absolute";
-    var index = items.indexOf(e.target);
-    e.target.classList.remove('dragable');
-    e.target.classList.add('dragging');
-
-    const data = {
-        id: e.target.id,
-        index
-    }
-    e.dataTransfer.setData('text/plain', JSON.stringify(data));
-}
-
-function isDraggingAbove(e) {
-    const elementRect = e.target.getBoundingClientRect();
-    const yElementPosition = elementRect.top + (e.target.offsetHeight / 2);
-    const yDragPosition = e.pageY;
-    return yDragPosition <= yElementPosition;
-}
-
-function dragEnter(e) {
-    dragOver(e);
-}
-
-function dragOver(e) {
-    cancelDefault(e);
-    if(isDraggingAbove(e)) {
-        e.target.classList.remove('moveBelow');
-        e.target.classList.add('moveAbove');
-    } else {
-        e.target.classList.remove('moveAbove');
-        e.target.classList.add('moveBelow');
-    }
-}
-
-function dragLeave(e) {
-    e.target.classList.remove('moveAbove');
-    e.target.classList.remove('moveBelow');
-    cancelDefault(e);
-}
-
-function dragDropped(e) {
-    let dragSource = JSON.parse(e.dataTransfer.getData('text/plain'));
-    dragSource.element = document.querySelector(`#${dragSource.id}`);
-
-    // remove any dragging CSS
-    dragSource.element.classList.remove('dragging');
-    dragSource.element.classList.add('dragable');
-    dragLeave(e);
-
-    // perform the move
-}
-
-function cancelDefault(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    return false;
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if(offset < 0 && offset > closest.offset) {
+            return {offset, element: child};
+        } else {
+            return closest;
+        }
+    }, {offset: Number.NEGATIVE_INFINITY}).element;
 }
