88f27a39aeb0cb15556cf8f7fd16cd1f8ad24a9f
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-08-07 15:05:36 -0400

    Testing complete

--- testing/script.js
+++ testing/script.js
@@ -1,15 +1,3 @@
-// elements = document.querySelectorAll('.draggable');
-// for(const element of elements) {
-//     element.draggable = true;
-//     element.addEventListener('dragstart', dragStart);
-//     element.addEventListener('drop', dragDropped);
-
-//     element.addEventListener('dragenter', dragEnter);
-//     element.addEventListener('dragover', dragOver);
-
-//     element.addEventListener('dragleave', dragLeave);
-// }
-
 const draggables = document.querySelectorAll('.draggable');
 const containers = document.querySelectorAll('.dropzone');
 
@@ -17,6 +5,7 @@ draggables.forEach(element => {
     element.draggable = true;
     element.addEventListener('dragstart', (event) => {
         element.classList.add('dragging');
+        dragSetData(event);
     });
     element.addEventListener('dragend', (event) => {
         element.classList.remove('dragging');
@@ -27,25 +16,39 @@ draggables.forEach(element => {
 containers.forEach(container => {
     container.addEventListener('dragover', (event) => {
         event.preventDefault();
-        const afterElement = getDragAfterElement(container, event.clientY);
+        const afterElement = getDragElementToPlaceBefore(container, event.clientY);
         const element = document.querySelector('.dragging');
-        if(afterElement == null) {
-            container.appendChild(element);
-        } else {
-            container.insertBefore(element, afterElement);
-        }
+        const index = [...container.querySelectorAll('.draggable')].indexOf(element);
+        if(afterElement == null) container.appendChild(element);
+        else container.insertBefore(element, afterElement);
+    });
+    container.addEventListener('drop', (event) => {
+        let dragResult = dragGetData(event);
+        const element = container.querySelector(`#${dragResult.id}`);
+        dragResult.endProjectID = container.id;
+        dragResult.endIndex = [...container.children].indexOf(element);
+        console.log(`FROM: project:${dragResult.startProjectID}, id:${dragResult.id}`);
+        console.log(`  TO: project:${dragResult.endProjectID}, position:${dragResult.endIndex}`);
     });
 });
 
-function getDragAfterElement(container, y) {
+function dragSetData(event) {
+    event.dataTransfer.setData('text/plain', JSON.stringify({
+            id: event.target.id,
+            startProjectID: event.target.parentElement.id,
+        }));
+}
+
+function dragGetData(event) {
+    return JSON.parse(event.dataTransfer.getData('text/plain'));
+}
+
+function getDragElementToPlaceBefore(container, y) {
     const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
     return draggableElements.reduce((closest, child) => {
         const box = child.getBoundingClientRect();
         const offset = y - box.top - box.height / 2;
-        if(offset < 0 && offset > closest.offset) {
-            return {offset, element: child};
-        } else {
-            return closest;
-        }
+        if(offset < 0 && offset > closest.offset) return {offset, element: child};
+        else return closest;
     }, {offset: Number.NEGATIVE_INFINITY}).element;
 }
