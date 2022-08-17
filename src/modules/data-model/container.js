bcb0b9a71d7f23230f679395ee02a86dcc61cadb
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-08-16 23:03:30 -0400

    Fix lint issues

--- src/modules/data-model/container.js
+++ src/modules/data-model/container.js
@@ -1,51 +1,57 @@
 export default class Container {
-    list;
+  list;
 
-    constructor() {
-        this.list = [];
+  constructor() {
+    this.list = [];
+  }
+
+  // returns: true or false
+  contains(key, value) {
+    if (key === null || value === null) return false;
+    return this.list.find((o) => o[key] === value) !== undefined;
+  }
+
+  // return: success (true or false)
+  add(object, hasKey = null, equalToValue = null) {
+    // TODO: Check list for duplicate values
+    // this if statement doesn't do that lol
+    // if(this.contains(key, value)) return false;
+    if (hasKey !== null
+        && equalToValue !== null
+        && this.contains(hasKey, equalToValue)) {
+      return false;
     }
 
-    // returns: true or false
-    contains(key, value) {
-        if(key === null || value === null) return false;
-        return this.list.find(o => o[key] === value) !== undefined;
-    }
+    this.list.push(object);
+    return true;
+  }
 
-    // return: success (true or false)
-    add(object, hasKey=null, equalToValue=null) {
-        // TODO: Check list for duplicate values
-        // this if statement doesn't do that lol
-        // if(this.contains(key, value)) return false;
-        this.list.push(object);
-        return true;
-    }
+  insert(object, index) {
+    this.list.splice(index, 0, object);
+  }
 
-    insert(object, index) {
-        this.list.splice(index, 0, object);
-    }
+  // return: number of removed objects
+  delete(key, value) {
+    if (key === null || key === undefined) return false;
+    if (value === null || value === undefined) return false;
+    const oldLength = this.list.length;
+    this.list = this.list.filter((o) => o[key] !== value);
+    return (oldLength - this.list.length) > 0;
+  }
 
-    // return: number of removed objects
-    delete(key, value) {
-        if(key === null || key === undefined) return false;
-        if(value === null || value === undefined) return false;
-        const oldLength = this.list.length;
-        this.list = this.list.filter(o => o[key] !== value);
-        return (oldLength - this.list.length) > 0;
-    }
+  get(key, value) {
+    return this.list.find((o) => o[key] === value);
+  }
 
-    get(key, value) {
-        return this.list.find(o => o[key] === value);
-    }
+  getAll() {
+    return this.list;
+  }
 
-    getAll() {
-        return this.list;
-    }
+  erase() {
+    this.list = [];
+  }
 
-    erase() {
-        this.list = [];
-    }
-
-    isEmpty() {
-        return this.list.length === 0;
-    }
+  isEmpty() {
+    return this.list.length === 0;
+  }
 }
