f9db20a97b15ef93c39210785cd4a8be53ad79fd
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-08-17 00:09:51 -0500

    Fix all eslint issues

--- src/modules/ui/message.js
+++ src/modules/ui/message.js
@@ -1,42 +1,41 @@
-import ID from "../data-model/id";
-import Item from "../data-model/item";
+import ID from '../data-model/id';
 
-let messageList = [];
-let id = new ID();
+const messageList = [];
+const id = new ID();
 
 export default class Message {
-    static createContainer(parent=document.body) {
-        const messageDiv = document.createElement('div');
-        messageDiv.id = 'message';
-        parent.append(messageDiv);
-    }
+  static createContainer(parent = document.body) {
+    const messageDiv = document.createElement('div');
+    messageDiv.id = 'message';
+    parent.append(messageDiv);
+  }
 
-    static createMessage(text) {
-        const containerDiv = document.querySelector('#message');
+  static createMessage(text) {
+    const containerDiv = document.querySelector('#message');
 
-        const messageDiv = document.createElement('div');
-        messageDiv.id = `message${id.next()}`;
-        messageDiv.classList.add('message');
+    const messageDiv = document.createElement('div');
+    messageDiv.id = `message${id.next()}`;
+    messageDiv.classList.add('message');
 
-        const buttonDelete = document.createElement('button');
-        buttonDelete.id = `buttonDelete${messageDiv.id}`
-        buttonDelete.classList.add('material-icons');
-        buttonDelete.classList.add('warning');
-        buttonDelete.textContent = "warning";
-        buttonDelete.addEventListener('click', () => messageDiv.remove());
+    const buttonDelete = document.createElement('button');
+    buttonDelete.id = `buttonDelete${messageDiv.id}`;
+    buttonDelete.classList.add('material-icons');
+    buttonDelete.classList.add('warning');
+    buttonDelete.textContent = 'warning';
+    buttonDelete.addEventListener('click', () => messageDiv.remove());
 
-        const message = document.createElement('label');
-        message.htmlFor = buttonDelete.id;
-        message.textContent = text;
+    const message = document.createElement('label');
+    message.htmlFor = buttonDelete.id;
+    message.textContent = text;
 
-        messageDiv.append(message);
-        messageDiv.append(buttonDelete);
+    messageDiv.append(message);
+    messageDiv.append(buttonDelete);
 
-        containerDiv.append(messageDiv);
-        messageList.push(messageDiv);
-    }
+    containerDiv.append(messageDiv);
+    messageList.push(messageDiv);
+  }
 
-    static notify(message) {
-        Message.createMessage(message);
-    }
+  static notify(message) {
+    Message.createMessage(message);
+  }
 }
