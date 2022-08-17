94f927c1c48c0f9132e9a0c2afa9139090122079
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-08-17 00:04:20 -0500

    Fix eslint issues

--- src/modules/data-model/todo.js
+++ src/modules/data-model/todo.js
@@ -1,26 +1,24 @@
-import Container from "./container";
-import Project from "./project";
-import Item from "./item";
+import Container from './container';
+import Project from './project';
 
 // A collection of "Project" objects
 export default class TODO extends Container {
-    constructor(data) {
-        super();
-        if (typeof data === 'string' || data instanceof String) throw "todo.js: Object data expected";
-        if(data === null) data = [];
+  constructor(todo) {
+    super();
+    const todoObject = todo ?? [];
+    if (typeof todoObject === 'string'
+    || todoObject instanceof String) throw new Error('todo.js: Object data expected');
 
-        // Iterate the project list and add the loaded items
-        for(const key in data.list) {
-            this.add(new Project(data.list[key]));
-        }
+    // Iterate the project list and add the loaded items
+    todoObject.list.forEach((project) => this.add(new Project(project)));
 
-        // Ensure a 'default' TODO exists.
-        if(!this.contains('id', 0)) this.add(new Project());
-    }
+    // Ensure a 'default' TODO exists.
+    if (!this.contains('id', 0)) this.add(new Project());
+  }
 
-    toJSON() {
-        return {
-            list: this.list,
-        };
-    }
+  toJSON() {
+    return {
+      list: this.list,
+    };
+  }
 }
